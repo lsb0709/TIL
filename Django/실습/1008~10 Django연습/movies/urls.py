17087d402f9e78f1cb013e87ae98cf614128cad8
lsb0709 <dltkdqor0709@gmail.com>
2022-10-10 22:09:16 +0900

    1010djagno

--- Django/실습/1008~10 Django연습/movies/urls.py
+++ Django/실습/1008~10 Django연습/movies/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'movies'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('main/', views.main, name='main'),
+    path('index/', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/update/', views.update, name='update'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
 ]
