5dab869c99ed4b13c84bcbed3f36ef8b4a498ea0
lsb0709 <dltkdqor0709@gmail.com>
2022-10-20 23:28:33 +0900

    1020총실습

---
+++ Django/실습/1020/articles/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render, redirect
+from .forms import ArticleForm, CommentForm
+from .models import Article, Comment
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    context = {
+        'articles':articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+def detail(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    comment_form = CommentForm()
+    comments = article.comment_set.all()
+    context = {
+        'article': article,
+        'comment_form': comment_form,
+        'comments': comments,
+    }
+    return render(request, 'articles/detail.html', context)
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:index')
+    else:
+        article_form = ArticleForm()
+    context = {
+        'article_form':article_form,
+    }
+    return render(request, 'articles/create.html', context)
+
+@login_required
+def comments_create(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment.user = request.user
+        comment.save()
+    return redirect('articles:detail', article.pk)
+
+@login_required
+def update(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST, instance=article)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        article_form=ArticleForm(instance=article)
+    context = {
+        'article_form': article_form,
+    }
+    return render(request, 'articles/update.html', context)
+
+@login_required
+def delete(request, article_pk):
+    Article.objects.get(pk=article_pk).delete()
+    return redirect('articles:index')
+
+def comments_delete(request, article_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if request.user == comment.user:
+        comment.delete()
+    return redirect('articles:detail', article_pk)
+
+
